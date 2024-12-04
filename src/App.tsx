07826a77540350eff942d42d73b34df628be10cf
Ayushi <ayushisaxena24111999@gmail.com>
2024-12-04 16:13:37 +0530

    fixed app issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Notes from "./components/Layout/Notes";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 // import Main from "./pages/Main";
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mainpage" element={<Layout />} />
-          <Route path="/notes" element={<Notes />} />
           <Route path="/errorpage" element={<ErrorPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
